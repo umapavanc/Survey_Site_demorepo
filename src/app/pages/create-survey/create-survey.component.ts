5015e640d058c957bd59fcd6443e57f9c0749571
Yasaman Barzegar <77850597+yas95b@users.noreply.github.com>
2021-11-15 06:43:26 -0500

    Update create-survey.component.ts

--- src/app/pages/create-survey/create-survey.component.ts
+++ src/app/pages/create-survey/create-survey.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-survey',
@@ -8,12 +9,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./create-survey.component.css']
 })
 export class CreateSurveyComponent extends BasePageComponent implements OnInit {
-
-  constructor(route: ActivatedRoute) {
+  ActivatedRoute: any;
+  constructor(
+    route: ActivatedRoute, 
+    private formBuilder: FormBuilder) {  
     super(route);
    }
 
-  override ngOnInit(): void {
-  }
+   myForm!: FormGroup;
+
+   override ngOnInit(){
+     this.myForm = this.formBuilder.group({
+       id: [],
+       question: ['', Validators.required],
+       answer: ['', Validators.required]
+     });
+   }
+
+  onSubmit() {
+    this.ActivatedRoute.navigate(['active-surveys']);
+  }  
 
 }
