1525ddf90ec4ad8f0bf211225ef7710555982de0
Uma Pavan Kumar <umapavanc@gmail.com>
2021-11-14 23:08:29 -0500

    Beautification in Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ActiveSurveysComponent } from './pages/active-surveys/active-surveys.component';
 import { BasePageComponent } from './partials/base-page/base-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LandingPageComponent,
     CreateSurveyComponent,
     LoginComponent,
     ActiveSurveysComponent,
-    BasePageComponent
+    BasePageComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
