f67b24ce73e397fe0030c1586685535cc1a05ced
Uma Pavan Kumar <umapavanc@gmail.com>
2021-11-15 18:08:33 -0500

    Last commit for the day#15112021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ActiveSurveysComponent } from './pages/active-surveys/active-surveys.co
 import { BasePageComponent } from './partials/base-page/base-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,10 @@ import { AboutComponent } from './pages/about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([
+      AboutComponent
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
