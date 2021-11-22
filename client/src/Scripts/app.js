93244192f40b5103d68b978b5373698d1f2f8240
Uma Pavan Kumar <umapavanc@gmail.com>
2021-11-22 04:13:01 -0500

    Final Merge

---
+++ client/src/Scripts/app.js
@@ -0,0 +1,14 @@
+/*
+Team Name - Quick Survey
+Group no - 2 
+Student Id's - 300566849, 301211038, 301153525, 301072907, 301159366
+*/
+(() => {
+
+    function Start()
+    {
+        console.log('App Started...');
+    }
+
+    window.addEventListener('load', Start)
+})();
