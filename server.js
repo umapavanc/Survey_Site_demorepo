bc91f0ba188ca5b1f98544be2c564709f6ddc715
Uma Pavan Kumar <umapavanc@gmail.com>
2021-11-21 22:10:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
   //origin: "http://localhost:8081"
 //};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
