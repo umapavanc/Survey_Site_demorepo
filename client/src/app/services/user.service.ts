a3d66b54599526f9713fb2c1d65dc72333c65931
Uma Pavan Kumar <umapavanc@gmail.com>
2021-11-28 17:07:07 -0500

    Modified the service files to connect APIserver

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'https://survey-server-group.herokuapp.com/api/test/';
+//'http://localhost:8080/api/test/';
 
 @Injectable({
   providedIn: 'root'
