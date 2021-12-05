44446ebeb8c07cc45d7b60c56a66266462daa089
Uma Pavan Kumar <umapavanc@gmail.com>
2021-12-05 02:06:05 -0500

    Fixed main navbar toggle, modified UI in login & register and added question UI

--- src/app/pages/survey-create/survey-create.component.ts
+++ src/app/pages/survey-create/survey-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SurveyService } from 'src/app/services/survey.service';
 import { Survey } from 'src/app/models/survey.model';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-survey-create',
@@ -8,7 +9,14 @@ import { Survey } from 'src/app/models/survey.model';
   styleUrls: ['./survey-create.component.css']
 })
 export class SurveyCreateComponent implements OnInit {
-
+  isButtonVisible:boolean = false;
+  question: string = "";
+  selectedType: string = "0";
+  checkArray = new Array(1);
+  isShownText: boolean = false;
+  isShownRadioButn: boolean = false;
+  isShownCheckBox: boolean = false;
+  isShownCommentBox: boolean = false;
   survey: Survey = {
     title: '',
     description: '',
@@ -36,5 +44,67 @@ export class SurveyCreateComponent implements OnInit {
         error => {
           console.log(error);
         });
+  }
+  onChange(value) {
+    if (value == "1") {
+      this.isShownText = true;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+    }
+    else if (value == "2") {
+      this.isShownRadioButn = true;
+      this.isShownText = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+    }
+    else if (value == "3") {
+      this.isShownCheckBox = true;
+      this.isShownRadioButn = false;
+      this.isShownText = false;
+      this.isShownCommentBox = false;
+    }
+    else if (value == "4") {
+      this.isShownCommentBox = true;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownText = false;
+    }
+    else {
+      this.isShownText = false;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+    }
+    console.log(value);
+  }
+
+  add() {
+    if (this.checkArray.length < 5) {
+      this.checkArray.length++;
+      console.log(this.checkArray.length);
+    }
+  }
+
+  remove() {
+    if (this.checkArray.length > 1) {
+      this.checkArray.length--;
+      console.log(this.checkArray.length);
+    }
+  }
+
+  saveQuestion(question) { 
+    this.isButtonVisible = true;
+    console.log("Hello" + question + " " + this.selectedType);
+  }
+
+  newQuestion(){
+    this.isButtonVisible = false;
+    this.selectedType = "0";
+    this.question = null;
+    this.isShownText = false;
+    this.isShownRadioButn = false;
+    this.isShownCheckBox = false;
+    this.isShownCommentBox = false;
   }
 }
