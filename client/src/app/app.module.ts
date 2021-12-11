8ac38696c18609e69bfb919125ce66a7e66e8cdf
barnau21 <adumitriu21@gmail.com>
2021-12-11 17:51:05 +0200

    added qquestion-create module, hooked it up to the back end, working on a way to capure surveyID

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
@@ -18,7 +20,10 @@ import { SurveyEditComponent } from './pages/survey-edit/survey-edit.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
-import {authInterceptorProviders} from './helpers/auth.interceptor'
+import {authInterceptorProviders} from './helpers/auth.interceptor';
+import { StarRatingComponent } from './star-rating/star-rating.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuestionCreateComponent } from './pages/question-create/question-create.component'
 
 @NgModule({
   declarations: [
@@ -33,7 +38,9 @@ import {authInterceptorProviders} from './helpers/auth.interceptor'
     SurveyListComponent,
     SurveyEditComponent,
     RegisterComponent,
-    ProfileComponent
+    ProfileComponent,
+    StarRatingComponent,
+    QuestionCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -42,9 +49,12 @@ import {authInterceptorProviders} from './helpers/auth.interceptor'
     HttpClientModule,
     AgGridModule.withComponents([
       AboutComponent
-    ])
+    ]),
+    BrowserAnimationsModule,
+    
   ],
   providers: [authInterceptorProviders],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
--- client/src/app/models/question.model.ts
+++ client/src/app/models/question.model.ts
@@ -6,5 +6,6 @@ Student Id's - 300566849, 301211038, 301153525, 301072907, 301159366
 export class Question{
     id?: string;
     questionText?: string;
+    questionType?: number;
     surveyId?: string;
 }
