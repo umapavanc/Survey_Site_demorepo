a14ad0254da88b25d1d6962721628b555780bbfd
barnau21 <adumitriu21@gmail.com>
2021-12-12 12:26:40 +0200

    small modifications to UI

--- client/src/app/pages/survey-create/survey-create.component.ts
+++ client/src/app/pages/survey-create/survey-create.component.ts
@@ -3,6 +3,7 @@ import { SurveyService } from 'src/app/services/survey.service';
 import { Survey } from 'src/app/models/survey.model';
 import { Question } from 'src/app/models/question.model';
 import { QuestionService } from 'src/app/services/question.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-survey-create',
@@ -10,29 +11,22 @@ import { QuestionService } from 'src/app/services/question.service';
   styleUrls: ['./survey-create.component.css']
 })
 export class SurveyCreateComponent implements OnInit {
-
-
-  // SURVEY IMPLEMENTATION 
-
+  
   survey: Survey = {
     title: '',
     description: '',
     published: false
   };
+
   surveySubmitted = false;
-  questionSubmitted = false;
-
   storedId : string;
-  
-
-  get passSurveyId(){
-    return this.storedId;
-  }
-
+ 
 
   constructor(private surveyService: SurveyService,
-    private questionService: QuestionService) { }
- 
+    private questionService: QuestionService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
   ngOnInit(): void {
   }
 
@@ -53,152 +47,144 @@ export class SurveyCreateComponent implements OnInit {
         error => {
           console.log(error);
         });
-
-        
   }
+ // QUESTION IMPLEMENTATION
+ question: Question ={
+  questionText: '',
+  questionType: 0,
+  surveyId: ''
+}
 
-  // QUESTION IMPLEMENTATION
-  question: Question ={
-    questionText: '',
-    questionType: 0,
-    surveyId: ''
+questionSubmitted = false;
+
+questions?: Question[];
+currentQuestion?: Question;
+currentIndex = -1;
+
+isButtonVisible:boolean = false;
+selectedType: string = "0";
+checkArray = new Array(1);
+radioArray = new Array(1);
+questionArray = new Array();
+isShownText: boolean = false;
+isShownRadioButn: boolean = false;
+isShownCheckBox: boolean = false;
+isShownCommentBox: boolean = false;
+isShownStarRating: boolean = false;
+
+onChange(value) {
+  if (value == "1") {
+    this.isShownText = true;
+    this.isShownRadioButn = false;
+    this.isShownCheckBox = false;
+    this.isShownCommentBox = false;
+    this.isShownStarRating = false;
   }
- 
-
-  questions?: Question[];
-  currentQuestion?: Question;
-  currentIndex = -1;
-
-  isButtonVisible:boolean = false;
-  selectedType: string = "0";
-  checkArray = new Array(1);
-  radioArray = new Array(1);
-  questionArray = new Array();
-  isShownText: boolean = false;
-  isShownRadioButn: boolean = false;
-  isShownCheckBox: boolean = false;
-  isShownCommentBox: boolean = false;
-  isShownStarRating: boolean = false;
- 
-   
-
-  
-  onChange(value) {
-    if (value == "1") {
-      this.isShownText = true;
-      this.isShownRadioButn = false;
-      this.isShownCheckBox = false;
-      this.isShownCommentBox = false;
-      this.isShownStarRating = false;
-    }
-    else if (value == "2") {
-      this.isShownRadioButn = true;
-      this.isShownText = false;
-      this.isShownCheckBox = false;
-      this.isShownCommentBox = false;
-      this.isShownStarRating = false;
-    }
-    else if (value == "3") {
-      this.isShownCheckBox = true;
-      this.isShownRadioButn = false;
-      this.isShownText = false;
-      this.isShownCommentBox = false;
-      this.isShownStarRating = false;
-    }
-    else if (value == "4") {
-      this.isShownCommentBox = true;
-      this.isShownRadioButn = false;
-      this.isShownCheckBox = false;
-      this.isShownText = false;
-      this.isShownStarRating = false;
-    }
-    else if (value == "5"){
-      this.isShownText = false;
-      this.isShownRadioButn = false;
-      this.isShownCheckBox = false;
-      this.isShownCommentBox = false;
-      this.isShownStarRating = true;
-    }
-  
-   
-    console.log(value);
+  else if (value == "2") {
+    this.isShownRadioButn = true;
+    this.isShownText = false;
+    this.isShownCheckBox = false;
+    this.isShownCommentBox = false;
+    this.isShownStarRating = false;
   }
-
-  addCheck() {
-    if (this.checkArray.length < 5) {
-      this.checkArray.length++;
-      console.log(this.checkArray.length);
-    }
+  else if (value == "3") {
+    this.isShownCheckBox = true;
+    this.isShownRadioButn = false;
+    this.isShownText = false;
+    this.isShownCommentBox = false;
+    this.isShownStarRating = false;
   }
-
-  removeCheck() {
-    if (this.checkArray.length > 1) {
-      this.checkArray.length--;
-      console.log(this.checkArray.length);
-    }
+  else if (value == "4") {
+    this.isShownCommentBox = true;
+    this.isShownRadioButn = false;
+    this.isShownCheckBox = false;
+    this.isShownText = false;
+    this.isShownStarRating = false;
   }
-
-  addRadio() {
-    if (this.radioArray.length < 5) {
-      this.radioArray.length++;
-      console.log(this.radioArray.length);
-    }
-  }
-
-  removeRadio() {
-    if (this.radioArray.length > 1) {
-      this.radioArray.length--;
-      console.log(this.radioArray.length);
-    }
-  }
-
-  addQuestion() {
-    if (this.questionArray.length < 10) {
-      this.questionArray.length++;
-      console.log(this.questionArray.length);
-    }
-  }
-
-  removeQuestion() {
-    if (this.questionArray.length > 1) {
-      this.questionArray.length--;
-      console.log(this.questionArray.length);
-    }
-  }
-
-  newQuestion(){
-    this.isButtonVisible = false;
-    this.selectedType = "0";
-    this.question = null;
+  else if (value == "5"){
     this.isShownText = false;
     this.isShownRadioButn = false;
     this.isShownCheckBox = false;
     this.isShownCommentBox = false;
+    this.isShownStarRating = true;
   }
 
-  saveQuestion(): void {
-    const data = {
-      questionText: this.question.questionText,
-      questionType: this.question.questionType,
-      surveyId: this.storedId
-    };
+ 
+  console.log(value);
+}
 
-    this.questionService.create(data)
-      .subscribe(
-        response => {
-          console.log(response);
-          this.questionSubmitted = true;
-        },
-        error => {
-          console.log(error);
-        });
+addCheck() {
+  if (this.checkArray.length < 5) {
+    this.checkArray.length++;
+    console.log(this.checkArray.length);
   }
+}
 
-  setCurrentQuestion(question: Question, index: number): void {
-    this.currentQuestion = question;
-    this.currentIndex = index;
+removeCheck() {
+  if (this.checkArray.length > 1) {
+    this.checkArray.length--;
+    console.log(this.checkArray.length);
   }
+}
 
-  
-  
+addRadio() {
+  if (this.radioArray.length < 5) {
+    this.radioArray.length++;
+    console.log(this.radioArray.length);
+  }
+}
+
+removeRadio() {
+  if (this.radioArray.length > 1) {
+    this.radioArray.length--;
+    console.log(this.radioArray.length);
+  }
+}
+
+addQuestion() {
+  if (this.questionArray.length < 10) {
+    this.questionArray.length++;
+    console.log(this.questionArray.length);
+  }
+}
+
+removeQuestion() {
+  if (this.questionArray.length > 1) {
+    this.questionArray.length--;
+    console.log(this.questionArray.length);
+  }
+}
+
+newQuestion(){
+  this.isButtonVisible = false;
+  this.selectedType = "0";
+  this.question = null;
+  this.isShownText = false;
+  this.isShownRadioButn = false;
+  this.isShownCheckBox = false;
+  this.isShownCommentBox = false;
+}
+
+saveQuestion(): void {
+  const data = {
+    questionText: this.question.questionText,
+    questionType: this.question.questionType,
+    surveyId: this.storedId
+  };
+
+  this.questionService.create(data)
+    .subscribe(
+      response => {
+        console.log(response);
+        this.questionSubmitted = true;
+      },
+      error => {
+        console.log(error);
+      });
+}
+
+closeSurvey(): void  {
+  this.router.navigate(['/home']);
+}
+
 }
