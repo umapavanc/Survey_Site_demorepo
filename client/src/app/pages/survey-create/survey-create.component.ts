408a1d5c6a43600a18df11fee16d3bd6561a4173
barnau21 <adumitriu21@gmail.com>
2021-12-14 22:44:36 +0200

    merged changes from uma branch

--- client/src/app/pages/survey-create/survey-create.component.ts
+++ client/src/app/pages/survey-create/survey-create.component.ts
@@ -8,24 +8,56 @@ import { Response } from 'src/app/models/response.model';
 import { ResponseService } from 'src/app/services/response.service';
 import { NgForm } from '@angular/forms';
 
+
 @Component({
   selector: 'app-survey-create',
   templateUrl: './survey-create.component.html',
   styleUrls: ['./survey-create.component.css']
 })
 export class SurveyCreateComponent implements OnInit {
-  
+
+  arrayOfQuestions: Question[] = new Array();
+
   survey: Survey = {
     title: '',
     description: '',
     published: false
   };
+  // QUESTION IMPLEMENTATION
+  question: Question = {
+    questionText: '',
+    questionType: 0,
+    surveyId: ''
+  }
 
-  surveySubmitted = false;
-  storedId : string;
-  qId: string;
+  responses: Response = {
+    responseText: '',
+    questionId: ''
+  }
   
- 
+  
+  responseSubmitted = false;
+  surveySubmitted = false;
+  storedId: string;
+  qId: string;
+  questionSubmitted = false;
+
+  questions?: Question[];
+  currentQuestion?: Question;
+  currentIndex = -1;
+
+  btnDisable: boolean = true;
+  isShownQuestions: boolean = false;
+  isButtonVisible: boolean = false;
+  selectedType: string = "0";
+  checkArray = new Array(1);
+  radioArray = new Array(2);
+  questionArray = new Array();
+  isShownText: boolean = false;
+  isShownRadioButn: boolean = false;
+  isShownCheckBox: boolean = false;
+  isShownCommentBox: boolean = false;
+  isShownStarRating: boolean = false;
 
   constructor(private surveyService: SurveyService,
     private questionService: QuestionService,
@@ -49,157 +81,143 @@ export class SurveyCreateComponent implements OnInit {
           this.surveySubmitted = true;
           this.storedId = response.id;
           console.log("HERE IT IS" + this.storedId);
+          this.isShownQuestions = true;
         },
         error => {
           console.log(error);
         });
   }
 
- // QUESTION + RESPONSE IMPLEMENTATION
- question: Question ={
-  questionText: '',
-  questionType: 0,
-  surveyId: ''
-}
-
-responses: Response = {
-  responseText: '',
-  questionId: ''
-}
-
-questionSubmitted = false;
-responseSubmitted = false;
-
-questions?: Question[];
-currentQuestion?: Question;
-currentIndex = -1;
-
-isButtonVisible:boolean = false;
-selectedType: string = "0";
-checkArray = new Array(1);
-radioArray = new Array(1);
-questionArray = new Array();
-isShownText: boolean = false;
-isShownRadioButn: boolean = false;
-isShownCheckBox: boolean = false;
-isShownCommentBox: boolean = false;
-isShownStarRating: boolean = false;
-
-onChange(value) {
-  if (value == "1") {
-    this.isShownText = true;
-    this.isShownRadioButn = false;
-    this.isShownCheckBox = false;
-    this.isShownCommentBox = false;
-    this.isShownStarRating = false;
-  }
-  else if (value == "2") {
-    this.isShownRadioButn = true;
-    this.isShownText = false;
-    this.isShownCheckBox = false;
-    this.isShownCommentBox = false;
-    this.isShownStarRating = false;
-  }
-  else if (value == "3") {
-    this.isShownCheckBox = true;
-    this.isShownRadioButn = false;
-    this.isShownText = false;
-    this.isShownCommentBox = false;
-    this.isShownStarRating = false;
-  }
-  else if (value == "4") {
-    this.isShownCommentBox = true;
-    this.isShownRadioButn = false;
-    this.isShownCheckBox = false;
-    this.isShownText = false;
-    this.isShownStarRating = false;
-  }
-  else if (value == "5"){
-    this.isShownText = false;
-    this.isShownRadioButn = false;
-    this.isShownCheckBox = false;
-    this.isShownCommentBox = false;
-    this.isShownStarRating = true;
-  }
-
- 
-  console.log(value);
-}
-
-addCheck() {
-  if (this.checkArray.length < 5) {
-    this.checkArray.length++;
-    console.log(this.checkArray.length);
-  }
-}
-
-removeCheck() {
-  if (this.checkArray.length > 1) {
-    this.checkArray.length--;
-    console.log(this.checkArray.length);
-  }
-}
-
-addRadio() {
-  if (this.radioArray.length < 5) {
-    this.radioArray.length++;
-    console.log(this.radioArray.length);
-  }
-}
-
-removeRadio() {
-  if (this.radioArray.length > 1) {
-    this.radioArray.length--;
-    console.log(this.radioArray.length);
-  }
-}
-
-addQuestion() {
-  if (this.questionArray.length < 10) {
-    this.questionArray.length++;
-    console.log(this.questionArray.length);
-  }
-}
-
-removeQuestion() {
-  if (this.questionArray.length > 1) {
-    this.questionArray.length--;
-    console.log(this.questionArray.length);
-  }
-}
-
-newQuestion(){
-  this.isButtonVisible = false;
-  this.selectedType = "0";
-  this.question = null;
-  this.isShownText = false;
-  this.isShownRadioButn = false;
-  this.isShownCheckBox = false;
-  this.isShownCommentBox = false;
-}
-
-saveQuestion(): void {
   
-  const questionData = {
-    questionText: this.question.questionText,
-    questionType: this.question.questionType,
-    surveyId: this.storedId
-  };
 
-  this.questionService.create(questionData)
-    .subscribe(
-      response => {
-        console.log(response);
-        this.questionSubmitted = true;
-        this.qId = response.id;
-        console.log(this.qId);
-      },
-      error => {
-        console.log(error);
-      });
-}
+  newQuestion() {
+    this.isButtonVisible = false;
+    //this.selectedType = "0";
+   // this.question = null;
+    this.isShownText = false;
+    this.isShownRadioButn = false;
+    this.isShownCheckBox = false;
+    this.isShownCommentBox = false;
+  }
 
-saveAnswer(): void {
+  saveQuestion(): void {
+    if (this.arrayOfQuestions == null) {
+      this.arrayOfQuestions = new Array();
+      console.log("Created");
+    }
+    if (this.question.questionText != '' && this.question.questionType != 0) {
+      const data = {
+        questionText: this.question.questionText,
+        questionType: this.question.questionType,
+        surveyId: this.storedId
+      };
+      this.arrayOfQuestions.push(data);
+      this.question.questionText = '';
+      this.question.questionType = 0;
+      this.newQuestion();
+      console.log(data)
+      console.log(this.arrayOfQuestions)
+    }
+
+    /**/
+  }
+
+  closeSurvey(): void {
+    for (var i = 0; i < this.arrayOfQuestions.length; i++) {
+      this.questionService.create(this.arrayOfQuestions[i])
+        .subscribe(
+          response => {
+            console.log(response.id);
+            this.questionSubmitted = true;
+            this.router.navigate(['/surveys']);
+          },
+          error => {
+            console.log(error);
+          });
+    }
+  }
+
+  enableSaveBtn(value) {
+    if (value != '' && this.question.questionType != 0) {
+      this.btnDisable = false;
+    }
+    else{
+      this.btnDisable = true;
+    }
+  }
+
+  onChange(value) {
+    if (this.question.questionText != '' && value != "0") {
+      this.btnDisable = false;
+    }else{
+      this.btnDisable = true;
+    }
+    if (value == "1") {
+      this.isShownText = true;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+      this.isShownStarRating = false;
+    }
+    else if (value == "2") {
+      this.isShownRadioButn = true;
+      this.isShownText = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+      this.isShownStarRating = false;
+    }
+    else if (value == "3") {
+      this.isShownCheckBox = true;
+      this.isShownRadioButn = false;
+      this.isShownText = false;
+      this.isShownCommentBox = false;
+      this.isShownStarRating = false;
+    }
+    else if (value == "4") {
+      this.isShownCommentBox = true;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownText = false;
+      this.isShownStarRating = false;
+    }
+    else if (value == "5") {
+      this.isShownText = false;
+      this.isShownRadioButn = false;
+      this.isShownCheckBox = false;
+      this.isShownCommentBox = false;
+      this.isShownStarRating = true;
+    }
+    console.log(value);
+  }
+
+  addCheck() {
+    if (this.checkArray.length < 5) {
+      this.checkArray.length++;
+      console.log(this.checkArray.length);
+    }
+  }
+
+  removeCheck() {
+    if (this.checkArray.length > 1) {
+      this.checkArray.length--;
+      console.log(this.checkArray.length);
+    }
+  }
+
+  addRadio() {
+    if (this.radioArray.length < 5) {
+      this.radioArray.length++;
+      console.log(this.radioArray.length);
+    }
+  }
+
+  removeRadio() {
+    if (this.radioArray.length > 1) {
+      this.radioArray.length--;
+      console.log(this.radioArray.length);
+    }
+  }
+  saveAnswer(): void {
     const resData = {
       responseText: this.responses.responseText,
       questionId: this.qId,
@@ -217,8 +235,6 @@ saveAnswer(): void {
       });
     }
 
-closeSurvey(): void  {
-  this.router.navigate(['/home']);
-}
 
+  
 }
