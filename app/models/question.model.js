f54b67f5dce7f5f73eac7715f547263f8406283e
Uma Pavan Kumar <umapavanc@gmail.com>
2021-12-14 18:41:51 -0500

    Adrian changes on question.routes.js file

--- app/models/question.model.js
+++ app/models/question.model.js
@@ -11,9 +11,7 @@ module.exports = mongoose => {
             {
                 questionText: String,
                 questionType: Number,
-                surveyId: String,
-              
-              
+                surveyId: String
             }
           
         )
